268829be37d93e703dfc29b2c081b35c6d57145b
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-08-12 17:38:51 -0500

    Add type for app's page names

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,8 +1,13 @@
 export type Maybe<T> = T | null | undefined;
 
 export interface Meme {
-    name: string;
-    artist: string;
-    id: number;
-    comment?: string;
+	name: string;
+	artist: string;
+	id: number;
+	comment?: string;
 }
+
+export type PageName = "upload"
+	                 | "select-meme"
+	                 | "generating"
+	                 | "result";
